486abbc925f57a4531bd9b0c4100e5b3cad63e66
Flotuz <flotuz10@gmail.com>
2020-12-01 21:03:38 -0600

    Se sube a Git

--- src/App.js
+++ src/App.js
@@ -1,25 +1,150 @@
-import logo from './logo.svg';
 import './App.css';
 
-function App() {
+export default function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+
+      <textarea readOnly id="resultado"></textarea>
+
+
+      <div className="table">
+
+        <div className="row">
+          {btn("C")}
+          {btn("/")}
+          {btn("X")}
+          {btn("DEL")}
+        </div>
+
+        <div className="row">
+          {btn(7)}
+          {btn(8)}
+          {btn(9)}
+          {btn("-")}
+        </div>
+
+        <div className="row">
+          {btn(4)}
+          {btn(5)}
+          {btn(6)}
+          {btn("+")}
+        </div>
+
+        <div className="row">
+          {btn(1)}
+          {btn(2)}
+          {btn(3)}
+          {btn("=")}
+        </div>
+        <div className="row">
+          {btn(0)}
+        </div>
+
+      </div>
     </div>
   );
 }
 
-export default App;
+function btn(caracter) {
+  return (
+    <div className="cell">
+      <button onClick={() => { dameLetra(caracter) }}> {caracter}</button>
+    </div>
+  );
+}
+
+function calcular(a, operador, b) {
+  switch (operador) {
+    case '+':
+      c = a + b;
+      document.getElementById('resultado').innerHTML = c;
+      a = 0;
+      b = 0;
+      c = 0;
+      operador = '';
+      break;
+
+    case '-':
+      c = a - b;
+      document.getElementById('resultado').innerHTML = c;
+      a = 0;
+      b = 0;
+      c = 0
+      operador = '';
+      break;
+
+    case 'X':
+      c = a * b;
+      document.getElementById('resultado').innerHTML = c;
+      a = 0;
+      b = 0;
+      c = 0
+      operador = '';
+      break;
+
+    case '/':
+      c = a / b;
+      document.getElementById('resultado').innerHTML = c;
+      a = 0;
+      b = 0;
+      c = 0
+      operador = '';
+      break;
+
+
+    default:
+      break;
+  }
+}
+
+let operador = '';
+let a = 0;
+let b = 0;
+let c = 0;
+function dameLetra(valor) {
+
+  switch (valor) {
+    case 'C':
+      document.getElementById('resultado').innerHTML = '';
+      a = 0;
+      b = 0;
+      operador = '';
+      break;
+    case '/':
+      operador = '/';
+      a = parseFloat(document.getElementById('resultado').innerHTML);
+      document.getElementById('resultado').innerHTML = '';
+      break;
+    case 'X':
+      operador = 'X';
+      a = parseFloat(document.getElementById('resultado').innerHTML);
+      document.getElementById('resultado').innerHTML = '';
+      break;
+    case 'DEL':
+      //Borrar de adelante hacia atrás uno a uno
+      let ultimo = document.getElementById('resultado').innerHTML;
+      ultimo = ultimo.substr(0, ultimo.length - 1);
+      document.getElementById('resultado').innerHTML = ultimo;
+      break;
+    case '-':
+      operador = '-';
+      a = parseFloat(document.getElementById('resultado').innerHTML);
+      document.getElementById('resultado').innerHTML = '';
+      break;
+    case '+':
+      operador = '+';
+      a = parseFloat(document.getElementById('resultado').innerHTML);
+      document.getElementById('resultado').innerHTML = '';
+      break;
+    case '=':
+      b = parseFloat(document.getElementById('resultado').innerHTML);
+      document.getElementById('resultado').innerHTML = '';
+      calcular(a, operador, b);
+      break;
+    default:
+      document.getElementById('resultado').innerHTML += valor;
+      console.log(valor);
+      break;
+  }
+}
